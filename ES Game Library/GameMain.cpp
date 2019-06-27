605eb01ba7ed50c0649aa60b07ef0fb7a195fb51
さず 神原 <hibisyun430@gmail.com>
2019-06-27 09:24:28 +0900

    初期状態

---
+++ ES Game Library/GameMain.cpp
@@ -0,0 +1,61 @@
+// #include "Extension\DirectX11\DXGraphics11.hpp"
+#include "StdAfx.h"
+#include "GameMain.h"
+
+/// <summary>
+/// Allows the game to perform any initialization it needs to before starting to run.
+/// This is where it can query for any required services and load all of your content.
+/// Initialize will enumerate through any components and initialize them as well.
+/// </summary>
+bool GameMain::Initialize()
+{
+	// TODO: Add your initialization logic here
+	WindowTitle(_T("ES Game Library"));
+
+
+	return true;
+}
+
+/// <summary>
+/// Finalize will be called once per game and is the place to release
+/// all resource.
+/// </summary>
+void GameMain::Finalize()
+{
+	// TODO: Add your finalization logic here
+
+}
+
+/// <summary>
+/// Allows the game to run logic such as updating the world,
+/// checking for collisions, gathering input, and playing audio.
+/// </summary>
+/// <returns>
+/// Scene continued value.
+/// </returns>
+int GameMain::Update()
+{
+	// TODO: Add your update logic here
+
+
+	return 0;
+}
+
+/// <summary>
+/// This is called when the game should draw itself.
+/// </summary>
+void GameMain::Draw()
+{
+	// TODO: Add your drawing code here
+	GraphicsDevice.Clear(Color_CornflowerBlue);
+
+	GraphicsDevice.BeginScene();
+
+
+	SpriteBatch.Begin();
+
+
+	SpriteBatch.End();
+
+	GraphicsDevice.EndScene();
+}
